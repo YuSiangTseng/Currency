243f3ae02c5afc7328480e0d531ab131cee25d25
Chris Tseng <TsengYuSiang@Chrisde-MacBook.local>
2016-12-29 15:52:41 +0000

    creating new project

---
+++ CashConvert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CashConvert
+//
+//  Created by Chris Tseng on 29/12/2016.
+//  Copyright © 2016 Tseng Yu Siang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
